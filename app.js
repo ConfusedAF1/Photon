bb3dabbe684ef206ef59d4deca079a93665b4550
ConfusedAF1 <149064103+ConfusedAF1@users.noreply.github.com>
2023-10-26 17:29:24 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const auth = "563492ad6f91700001000001d53a167f72024fd492a5d737838c3747";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
+let searchValue;
+let page = 1;
+let fetchLink; 
+let currentSearch;
+
+//Event Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e)=> {
+    //Preventing Refreshing of the page
+    e.preventDefault();
+    currentSearch = searchValue;
+    searchPhotos(searchValue);
+});
+more.addEventListener("click", loadMore);
+
+//Functions
+
+function updateInput(e){
+    searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
+        method : "GET",
+        headers : {
+            Accept: "application/json",
+            Authorization : auth
+        }
+    });
+    const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data) {
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `<div class = "gallery-info"><p>${photo.photographer}</p>
+        <a href = ${photo.src.original}>Download 🡓</a></div> <img src =${photo.src.large}></img>`;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+async function curatedPhotos(){
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    clear();
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+function clear() {
+    gallery.innerHTML = "";
+    searchInput.value = "";
+}
+async function loadMore(){
+    page++;
+    if (currentSearch){
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+curatedPhotos();
